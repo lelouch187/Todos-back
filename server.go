269ee5b3bbff349175b431c59a1f310323abd332
lelouch187 <vanushs187@gmail.com>
2024-07-06 20:37:53 +0600

    init project

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"log"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	app.Get("/api/healthchecker", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"status":  "success",
+			"message": "Welcome to Golang, Fiber, and GORM",
+		})
+	})
+
+	log.Fatal(app.Listen(":8000"))
+}
